d03a4dacc9e3be9240c4e50bc373c1f07a44209e
Sadan Mallhi <smallhi@chathamfinancial.com>
2019-04-21 04:41:29 -0400

    debugging the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Copy Prod YAML File to VPS (update the yml to pull from docker hub)
-sshpass -p '$VPS_PASSWORD' scp ./docker-compose.prod.yml '$VPS_USERNAME'@'$VPS_IP':/root/tmp/docker-compose-portfolio.yml
+# sshpass -p "$VPS_PASSWORD" scp ./docker-compose.prod.yml "$VPS_USERNAME"@"$VPS_IP":/root/tmp/docker-compose-portfolio.yml
+sshpass -p "$VPS_PASSWOR" scp ./docker-compose.prod.y "$VPS_USERNAME"@"$VPS_IP":/root/tmp/docker-compose-portfolio.yml
 
 # SSH Login
 # sshpass -p $VPS_PASSWORD ssh $VPS_USER@$VPS_IP
