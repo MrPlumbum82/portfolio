201d0b0057b1aa60321cf9b3711972ea49fbd758
Sadan Mallhi <smallhi@chathamfinancial.com>
2019-04-21 04:55:12 -0400

    debugging the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Copy Prod YAML File to VPS (update the yml to pull from docker hub)
 # sshpass -p "$VPS_PASSWORD" scp ./docker-compose.prod.yml "$VPS_USERNAME"@"$VPS_IP":/root/tmp/docker-compose-portfolio.yml
-sshpass -p "$VPS_PASSWOR" scp ./docker-compose.prod.y "$VPS_USERNAME"@"$VPS_IP":/root/tmp/docker-compose-portfolio.yml
+sshpass -p "$VPS_PASSWOR" scp -o stricthostkeychecking=no ./docker-compose.prod.y "$VPS_USERNAME"@"$VPS_IP":/root/tmp/docker-compose-portfolio.yml
 
 # SSH Login
-# sshpass -p $VPS_PASSWORD ssh $VPS_USER@$VPS_IP
+# sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USER@$VPS_IP
 
 # Docker Login 
 # echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
