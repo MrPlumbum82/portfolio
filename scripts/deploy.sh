708b171bf1c22ae0cb254f2825b1db825d0912ac
Sadan Mallhi <smallhi@chathamfinancial.com>
2019-04-21 05:01:38 -0400

    lets see if the deploy scirpt works now

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Copy Prod YAML File to VPS (update the yml to pull from docker hub)
-# sshpass -p "$VPS_PASSWORD" scp ./docker-compose.prod.yml "$VPS_USERNAME"@"$VPS_IP":/root/tmp/docker-compose-portfolio.yml
-sshpass -p "$VPS_PASSWOR" scp -o stricthostkeychecking=no ./docker-compose.prod.y "$VPS_USERNAME"@"$VPS_IP":/root/tmp/docker-compose-portfolio.yml
+sshpass -p "$VPS_PASSWORD" scp -o stricthostkeychecking=no ./docker-compose.prod.yml "$VPS_USERNAME"@"$VPS_IP":/opt/tmp/docker-compose-portfolio.yml
 
 # SSH Login
-# sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USER@$VPS_IP
+sshpass -p "$VPS_PASSWORD" ssh -o StrictHostKeyChecking=no "$VPS_USER"@"$VPS_IP"
 
 # Docker Login 
-# echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Run Docker Compose
 echo "ALMOST DONE BOIS!"
 pwd
+cd /opt/tmp
 # docker-compose -f docker-compose-portfolio.yml up -d
