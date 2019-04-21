5370cd224d33f171c0c9f8f4a0108c9ced0faafd
Sadan Mallhi <smallhi@chathamfinancial.com>
2019-04-21 03:49:36 -0400

    testing the deploy process

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "yoyo lets deploy dis sheeeeet dawg!"
+# Copy Prod YAML File to VPS (update the yml to pull from docker hub)
+sshpass -p $VPS_PASSWORD scp ./docker-compose.prod.yml $VPS_USER@$VPS_IP:/root/tmp/docker-compose-portfolio.yml
+
+# SSH Login
+sshpass -p $VPS_PASSWORD ssh $VPS_USER@$VPS_IP
+
+# Docker Login 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Run Docker Compose
+echo "ALMOST DONE BOIS!"
+pwd
+# docker-compose -f docker-compose-portfolio.yml up -d
