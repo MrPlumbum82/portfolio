400a671177519da5eec665e6e4b17dbf35f4cb2c
Sadan Mallhi <smallhi@chathamfinancial.com>
2019-04-21 05:19:12 -0400

    env var bug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 sshpass -p $VPS_PASSWORD scp -o stricthostkeychecking=no ./docker-compose.prod.yml $VPS_USERNAME@$VPS_IP:/opt/tmp/docker-compose-portfolio.yml
 
 # SSH Login
-sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USER@$VPS_IP
+sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USERNAME@$VPS_IP
 
 # Docker Login 
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
