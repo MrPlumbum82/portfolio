198cdd8211df804bb871aa9c3158a21ff1e1dacc
Sadan Mallhi <smallhi@chathamfinancial.com>
2019-04-21 05:32:44 -0400

    testing the ssh connection

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,11 @@
 # Copy Prod YAML File to VPS (update the yml to pull from docker hub)
 sshpass -p $VPS_PASSWORD scp -o stricthostkeychecking=no ./docker-compose.prod.yml $VPS_USERNAME@$VPS_IP:/opt/tmp/docker-compose-portfolio.yml
 
-# SSH Login
-sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USERNAME@$VPS_IP
+# SSH Login and Docker Login
+sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USERNAME@$VPS_IP "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
 
-# Docker Login 
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+# Testing
+sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USERNAME@$VPS_IP "pwd"
 
 # Run Docker Compose
-echo "ALMOST DONE BOIS!"
-pwd
-cd /opt/tmp
-# docker-compose -f docker-compose-portfolio.yml up -d
+# sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USERNAME@$VPS_IP "docker-compose -f /opt/tmp/docker-compose-portfolio.yml up -d"
