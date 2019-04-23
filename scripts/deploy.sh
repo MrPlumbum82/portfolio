3f26a5ea1109cbea4dddd2f1a4edf12a7f53c796
Sadan Mallhi <smallhi@chathamfinancial.com>
2019-04-23 05:29:26 -0400

    trying to debug the docker image updating

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 # Copy Prod YAML File to VPS (update the yml to pull from docker hub)
 sshpass -p $VPS_PASSWORD scp -o stricthostkeychecking=no ./docker-compose.prod.yml $VPS_USERNAME@$VPS_IP:/opt/tmp/docker-compose-portfolio.yml
 
-# SSH Login and Docker Login
-sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USERNAME@$VPS_IP "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+# SSH Login and Docker Login (I probably don't need to login, just pulling)
+# sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USERNAME@$VPS_IP "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
 
 # Run Docker Compose
-sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USERNAME@$VPS_IP "docker-compose -f /opt/tmp/docker-compose-portfolio.yml up -d --force-recreate --no-deps --build"
+# sshpass -p $VPS_PASSWORD ssh -o StrictHostKeyChecking=no $VPS_USERNAME@$VPS_IP "docker-compose -f /opt/tmp/docker-compose-portfolio.yml up -d --force-recreate --no-deps --build"
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Login in to push to docker hub
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+
+# Push to docker hub with tag
 docker push mrplum/portfolio:latest
